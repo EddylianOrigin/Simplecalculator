f8f3ebe9bad5a2ab1ef500dd263d120f40608749
sylvaineddy <nguesson@rhrk.uni-kl.de>
2021-06-06 21:31:47 +0200

    release

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,10 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.util.Objects;
+
 
 public class Main extends Application {
 
--- src/sample/Model.java
+++ src/sample/Model.java
@@ -1,7 +1,7 @@
 package sample;
 
 public class Model {
-    public float Calculate(long number1 ,long number2, String operator) {
+    public float Calculate(float number1 ,float number2, String operator) {
         switch (operator) {
             case "+":
             return number1 + number2;
